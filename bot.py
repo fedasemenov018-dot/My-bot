65dbc9ac31eb6473d038d8198770ee6cad19c2db
fedasemenov018-dot <fedasemenov018@gmail.com>
2026-08-29 18:31:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
 import os
-import threading
 from flask import Flask
 
 app = Flask(__name__)
@@ -252,6 +251,5 @@ def get_time(message):
         print(f"Ошибка при отправке клиенту: {e}")
 
 if __name__ == "__main__":
-    import threading
-    threading.Thread(target=bot.polling, kwargs={'non_stop': True}).start()
+    bot.polling(none_stop=True)
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
