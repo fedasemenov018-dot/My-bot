309f345aa19da5b119be9cab5b898465dde5e2c2
fedasemenov018-dot <fedasemenov018@gmail.com>
2026-08-29 17:50:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -281,4 +281,4 @@ def get_time(message):
         print(f"Ошибка при отправке клиенту: {e}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))) 
